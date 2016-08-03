0c49d7423a70bbab3aba187914951043ba502692
Project Rexon <sourcecode1984@protonmail.com>
2016-08-03 17:50:07 +0530

    Create Hello_World.cpp

---
+++ Hello_World.cpp
@@ -0,0 +1,15 @@
+
+// Instructing the preprocessor to include I/O libraries
+#include <iostream>
+
+//Instructs the compiler to refer string and vector to std::string and std::vector respectively 
+using namespace std;
+
+// The main Function
+int main(){
+
+cout << "Hello World!";
+
+return 0;
+
+}
