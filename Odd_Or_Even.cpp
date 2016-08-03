672f35f64283c1795d692f1ee90bb5e3a481ba08
Project Rexon <sourcecode1984@protonmail.com>
2016-08-03 17:56:19 +0530

    Create Odd_Or_Even.cpp

---
+++ Odd_Or_Even.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+
+    cout << "Enter an integer: ";
+
+    cin >> x;
+
+    if ( n%2 == 0) {
+        cout << n << " is an even number.";
+    }
+    else {
+        cout << n << " is an odd number.";
+
+    }
+    return 0;
+}
